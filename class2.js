f295d56ce4a228a8b265470c05827917fa29f856
Ali Ayaz <160322443+Ali27Ayaz@users.noreply.github.com>
2024-03-14 20:36:43 +0500

    Add files via upload

---
+++ class2.js
@@ -0,0 +1,24 @@
+"use strict";
+// let fname= "My Name is: Ayaz Ali"
+// let Surname= "Shahani"
+// let Coursename= "Typescript"
+// let Remarks= "I really enjoying this Course, thanx to my Teacher's who taught me well"
+// console.log(   )
+// console.log(fname + " " + Surname)
+// console.log(   )
+// console.log("& My Course Name is " + Coursename)
+// console.log(   )
+// console.log(Remarks)
+// console.log(   )
+// let Fname: string= " My Name is: Ayaz Ali "
+// let Lname: string= " My Last name is: Shahani "
+// let Cname: string= " My Course is: "
+// let Cnam: string= " Type Script , Java Script"
+// let PersName: string= " Hello Sir, Zia Khan and its Whole Team,"
+// let coding: number= 12345
+// console.log (Fname)
+// console.log (Lname)
+// console.log(Cname + " " + Cnam)
+// console.log (PersName + " I am submitting this Exercise to You, plz guide if any mistake")
+// console.log(   )
+// console.log("My code is: " + coding)
